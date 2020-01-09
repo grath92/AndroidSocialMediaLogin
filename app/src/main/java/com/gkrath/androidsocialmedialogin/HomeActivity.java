1f49804c173634804042a643f109ba6c9e2dad3f
Gopal <admin@gopal.local>
2020-01-09 15:03:41 +0530

    Social SDK SetUp

--- app/src/main/java/com/gkrath/androidsocialmedialogin/HomeActivity.java
+++ app/src/main/java/com/gkrath/androidsocialmedialogin/HomeActivity.java
@@ -2,6 +2,7 @@ package com.gkrath.androidsocialmedialogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void gotoHomeScreen(){
+        Intent homeScreenIntent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeScreenIntent);
+        finish();
     }
 }
